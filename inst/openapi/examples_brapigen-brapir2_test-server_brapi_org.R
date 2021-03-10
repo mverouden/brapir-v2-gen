3e7a03c88685da8cf3e6b4a2bbed9fec804a3e36
Verouden, Maikel <maikel.verouden@gmail.com>
2021-03-10 13:33:05 +0100

    minor updates

--- inst/openapi/examples_brapigen-brapir2_test-server_brapi_org.R
+++ inst/openapi/examples_brapigen-brapir2_test-server_brapi_org.R
@@ -1,12 +1,14 @@
-## tested calls BrAPI-Core: GET 2, POST 0, PUT 0
-## out of: GET 23, POST 14, PUT 7
-##
 ## library(brapir)
 ## con <- brapi_db()$testserver
 ## class(con) <- c("list", "brapi", "brapi_con")
 ## con[["token"]] <- "YYYY"
 ##
-
-## result section: Detail
-brapi_get_commoncropnames(con = con)#, pageSize = 1, page = 1) # 1
-brapi_get_lists(con = con)#, listType = "", listName = "", listDbId = "", listSource = "", externalReferenceID = "", externalReferenceSource = "", page = 0, pageSize = 1000) # 2
+## tested calls BrAPI-Core: GET 2, POST 0, PUT 0
+## out of: GET 23, POST 14, PUT 7
+##
+## GET (D = Detail, M = Master, M/D = Master/Detail)
+brapi_get_commoncropnames(con = con)#, pageSize = 1, page = 1) # 1 (D)
+brapi_get_lists(con = con)#, listType = "", listName = "", listDbId = "", listSource = "", externalReferenceID = "", externalReferenceSource = "", page = 0, pageSize = 1000) # 2 (D)
+brapi_get_lists_listDbId(con = con, listDbId = "list1") # 3 (M/D)
+brapi_get_locations(con = con)#, locationType = "Storage location") # 4 (D)
+brapi_get_locations_locationDbId(con = con, locationDbId = "location_01") # 5 (M)
