e0fa27ccb235dada2306a1bb6841c792c17d02b4
Verouden, Maikel <maikel.verouden@gmail.com>
2021-04-14 21:14:11 +0200

    updated examples with 2 post calls

--- inst/openapi/examples_brapigen-brapir2_test-server_brapi_org.R
+++ inst/openapi/examples_brapigen-brapir2_test-server_brapi_org.R
@@ -3,7 +3,7 @@
 ## class(con) <- c("list", "brapi", "brapi_con")
 ## con[["token"]] <- "YYYY"
 ##
-## tested calls BrAPI-Core: GET 17/23, POST 0/14, PUT 0/7
+## tested calls BrAPI-Core: GET 17/23, POST 2/14, PUT 0/7
 ##
 ## GET (D = Detail, M = Master, M/D = Master/Detail)
 brapi_get_commoncropnames(con = con)#, pageSize = 1, page = 1) # 1 (D)
@@ -31,3 +31,6 @@ brapi_get_studies_studyDbId(con = con, studyDbId = "study1") # 20 (M)
 brapi_get_studytypes(con = con)#, page = 0, pageSize = 1000) # 21 (D)
 brapi_get_trials(con = con)#, active = NA, commonCropName = "", contactDbId = "", programDbId = "", locationDbId = "", searchDateRangeStart = "", searchDateRangeEnd = "", studyDbId = "", trialDbId = "", trialName = "", trialPUI = "", sortBy = "", sortOrder = "", externalReferenceID = "", externalReferenceSource = "", page = 0, pageSize = 1000) # 22 (D)
 brapi_get_trials_trialDbId(con = con, trialDbId = "trial3") # 23 (M)
+## POST
+brapi_post_lists(con = con, data = data, additionalInfo = additionalInfo, dateCreated = dateCreated, dateModified = dateModified, externalReferences = externalReferences, listDescription = listDescription, listName = listName, listOwnerName = listOwnerName, listOwnerPersonDbId = listOwnerPersonDbId, listSize = listSize, listSource = listSource, listType = listType) # 1 see Examples section for specification
+brapi_post_lists_listDbId_items(con = con, listDbId = unique(out$listDbId), items = c("germ1", "germ2", "germ3")) # 2 see Examples section for specification
